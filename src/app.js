d1a5a85bd7ef54b25924f9a7fc5ced6f58164efe
samofoke <samofoke@student.wethinkcode.co.za>
2020-05-25 19:54:16 +0200

    the get method

--- src/app.js
+++ src/app.js
@@ -9,3 +9,5 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index', {title: 'index'}));
